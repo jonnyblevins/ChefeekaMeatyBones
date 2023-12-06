ae2f06bf0359c667dbbadd4a267c9a6e4ab0ea14
jonny <blevinsjonny@gmail.com>
2023-12-06 12:44:16 -0500

    Still messing with card images.

--- script.js
+++ script.js
@@ -22,35 +22,49 @@ const frontImages = [
     'https://i.imgur.com/8kXNEZ7.jpg',
   ];
   
-  const flipCardContainer = document.getElementById('flipCardContainer');
+console.log('Script loaded'); // This will log a message as soon as the script is loaded
+
+const frontImages = [
+  'https://i.imgur.com/DwkiG4P.jpg',
+  // other URLs
+];
+
+const backImages = [
+  'https://i.imgur.com/v0kceC8.jpg',
+  // other URLs
+];
+
+const flipCardContainer = document.getElementById('flipCardContainer');
+
+for (let i = 0; i < 9; i++) {
+  console.log(`Creating card ${i + 1}`); // This will log a message for each card being created
+
+  const flipCard = document.createElement('div');
+  flipCard.classList.add('flip-card');
+
+  const flipCardInner = document.createElement('div');
+
+  flipCardInner.classList.add('flip-card-inner');
   
-  for (let i = 0; i < 9; i++) {
-    const flipCard = document.createElement('div');
-    flipCard.classList.add('flip-card');
-  
-    const flipCardFront = document.createElement('div');
-    flipCardFront.classList.add('flip-card-front');
-    const frontImg = document.createElement('img');
-    frontImg.src = frontImages[i % frontImages.length];
-    frontImg.alt = `Front Image ${i + 1}`;
-    flipCardFront.appendChild(frontImg);
-  
-    console.log('Front Image Element:', frontImg); // Debug log
-  
-    const flipCardBack = document.createElement('div');
-    flipCardBack.classList.add('flip-card-back');
-    const backImg = document.createElement('img');
-    backImg.src = backImages[i % backImages.length];
-    backImg.alt = `Back Image ${i + 1}`;
-    flipCardBack.appendChild(backImg);
-  
-    console.log('Back Image Element:', backImg); // Debug log
-  
-    flipCard.appendChild(flipCardFront);
-    flipCard.appendChild(flipCardBack);
-  
-    flipCardContainer.appendChild(flipCard);
-  }
+  const flipCardFront = document.createElement('div');
+  flipCardFront.classList.add('flip-card-front');
+  const frontImg = document.createElement('img');
+  frontImg.src = frontImages[i % frontImages.length];
+  frontImg.alt = `Front Image ${i + 1}`;
+  flipCardFront.appendChild(frontImg);
+
+  const flipCardBack = document.createElement('div');
+  flipCardBack.classList.add('flip-card-back');
+  const backImg = document.createElement('img');
+  backImg.src = backImages[i % backImages.length];
+  backImg.alt = `Back Image ${i + 1}`;
+  flipCardBack.appendChild(backImg);
+
+  flipCardInner.appendChild(flipCardFront);
+  flipCardInner.appendChild(flipCardBack);
+  flipCard.appendChild(flipCardInner);
+  flipCardContainer.appendChild(flipCard);
+}
   
   window.addEventListener('scroll', function() {
     const video = document.getElementById('videoContainer');
